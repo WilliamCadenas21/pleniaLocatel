24f06ed299116099a6b9df0e48befe4e6bed7ca7
jjsuarestra99 <jjsuarestra99@gmail.com>
2019-11-12 18:46:16 -0500

    Adding Audit, Accounting and Payment guis

--- routes/index.js
+++ routes/index.js
@@ -28,8 +28,21 @@ router.get('/franquicia/home', function(req, res){
 });
 
 router.get('/franquicia/auditoria', function(req, res){
-  res.render('franquicia_auditoria', {employees:[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]});
+  res.render('franquicia_auditoria', {employees:[[0, 1, 2, 3, 8, 5, 2, 7, 8, 9], [4, 1, 2, 6, 4, 5, 8, 8, 8, 9]]});
 });
 
+router.get('/franquicia/pagos', function(req, res){
+  res.render('franquicia_pagos', {payments: [
+    {amount: 4500000, paid: false, id: 1231238, type: 'order'}, 
+    {amount: 7500000, paid: true, id: 1231221, type: 'order'},
+    {amount: 2320000, paid: false, id: 1231235, type: 'order'},
+    {amount: 23000000, paid: true, id: 2453235, type: 'acc'},
+    {amount: 32350000, paid: false, id: 2531235, type: 'acc'}
+  ]});
+});
+
+router.get('/franquicia/contabilidad', function(req, res){
+  res.render('franquicia_contabilidad', {income: 4500000, expenses: 3500000, on_transit: [300000, 450000, 200000]});
+});
 
 module.exports = router;
