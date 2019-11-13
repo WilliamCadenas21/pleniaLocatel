83e398a23b94ee4a137a1e00f0ac60d6a2eee9b5
jjsuarestra99 <jjsuarestra99@gmail.com>
2019-11-13 06:38:47 -0500

    Adding Accounting, Orders and Inventories for franchises

--- routes/index.js
+++ routes/index.js
@@ -24,15 +24,15 @@ router.post('/login', function(req, res){
 });
 
 router.get('/franquicia/home', function(req, res){
-  res.render('franquicia_home');
+  res.render('franquicia/home');
 });
 
 router.get('/franquicia/auditoria', function(req, res){
-  res.render('franquicia_auditoria', {employees:[[0, 1, 2, 3, 8, 5, 2, 7, 8, 9], [4, 1, 2, 6, 4, 5, 8, 8, 8, 9]]});
+  res.render('franquicia/auditoria', {employees:[[0, 1, 2, 3, 8, 5, 2, 7, 8, 9], [4, 1, 2, 6, 4, 5, 8, 8, 8, 9]]});
 });
 
 router.get('/franquicia/pagos', function(req, res){
-  res.render('franquicia_pagos', {payments: [
+  res.render('franquicia/pagos', {payments: [
     {amount: 4500000, paid: false, id: 1231238, type: 'order'}, 
     {amount: 7500000, paid: true, id: 1231221, type: 'order'},
     {amount: 2320000, paid: false, id: 1231235, type: 'order'},
@@ -42,7 +42,28 @@ router.get('/franquicia/pagos', function(req, res){
 });
 
 router.get('/franquicia/contabilidad', function(req, res){
-  res.render('franquicia_contabilidad', {income: 4500000, expenses: 3500000, on_transit: [300000, 450000, 200000]});
+  res.render('franquicia/contabilidad', {income: 4500000, expenses: 3500000, on_transit: [300000, 450000, 200000]});
+});
+
+router.get('/franquicia/ordenes', function(req, res){
+  res.render('franquicia/ordenes', {products: [{name: 'Equipo 1'}, {name: 'Equipo 2'}]});
+});
+
+router.post('/franquicia/ordenes', function(req, res){
+  console.log(req.body);
+  res.end();
+});
+
+router.get('/franquicia/inventarios', function(req, res){
+  res.render('franquicia/inventarios', {stores: ['ABC', 'DEF']});
+});
+
+router.post('/franquicia/inventarios', function(req, res){
+  var store = req.body.store == 'own' ? 'Propio' : req.body.store;
+  res.render('franquicia/inventarios_ver', {store: store, products: [
+    {name: 'Equipo 1', quantity: 35},
+    {name: 'Equipo 2', quantity: 12},
+  ]});
 });
 
 module.exports = router;
